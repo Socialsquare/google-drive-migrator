488f667347b8ac0b4f24d16327a8d173766c8ed0
Malthe Jorgensen <malthe.jorgensen@gmail.com>
2015-02-04 11:47:11 +0100

    [Add] functionality: copy folder recursively
    Update README, .gitignore, requirements.txt etc.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# google-drive-migrator
 Google Drive Migrator
+=====================
+
+Google Drive Migrator is a Python script that recursively copies the contents of
+a Google Drive folder into another folder on Google Drive using the
+[Google Drive SDK][].
+
+_Note_: Before continuing you should consider whether [Google Takeout][] meets
+your needs.
+
+Prerequisites
+-------------
+The project uses Python 2.7 and `pip`.
+
+1. Grab the code `git clone git@github.com:socialsquare/google-drive-migrator.git`
+2. Install requirements `pip install -r requirements.txt`
+3. Get a Client ID and Secret Key 
+
+   1. Go to <https://console.developers.google.com/>
+   2. Create a project
+   3. Go to _APIs & auth_ and enable _Drive SDK_
+   4. Go to _APIs & auth_ -> _Credentials_ and then _Create new Client ID_
+      and create a  _Web application_, with a callback URI
+      `http://localhost:8080/`.
+
+      You should now see a Client ID and a Secret Key.
+
+   5. Click _Download JSON_ and save the JSON file as `client_secrets.json` in
+      the google-drive-migrator-folder.
+
+Running
+-------
+Run
+
+    python copy_new_owner.py GDRIVE_FOLDER_ID_FROM GDRIVE_FOLDER_ID_TO
+
+, where `GDRIVE_FOLDER_ID_FROM` and `GDRIVE_FOLDER_ID_TO` are Google Drive
+folder ids of the folder to be copied from and the folder to be copied to
+respectively e.g.
+
+    python copy_new_owner.py 'AaaadadOIDSAD22323' 'BDK-LFJfkajsadJAd231j'
+
+the Google Drive SDK will automatically open your webbrowser where you have to
+login to your Google account and authenticate the app you created.
+The authentication is saved in `saved_credentials.json` so that you don't have
+to login on subsequent uses.
+
+When you have authenticated in the browser (it shows a boring page with the text
+"the request has been authorized"), the script will start copying the folder.
+
+Best wishes!
+Malthe from Socialsquare
+
+[Google Drive SDK]: https://developers.google.com/drive/
+[Google Takeout]: https://google.com/takeout
---
+++ copy_folder.py
@@ -0,0 +1,117 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from pprint import pprint as pp
+
+
+def recursiveCopyInto(gauth, fID_from, fID_to, maxdepth=float('infinity'), __currentDepth=0):
+
+    if __currentDepth > maxdepth:
+        return
+
+    result = gauth.service.files().get(fileId=fID_from).execute()
+    print '  ' * __currentDepth + 'Recursively copying "%s" (id: %s)' % (result['title'], result['id'])
+    print '  ' * __currentDepth + 'into id: %s' % fID_to
+
+
+    # Go through children with pagination
+    while True:
+        result = gauth.service.files().list(q='"%s" in parents and trashed = false' % fID_from).execute()
+        # Alternative way to get children:
+        #   (returns `drive#childReference` instead of `drive#file`)
+        # result = gauth.service.children().list(folderId=fID_from).execute()
+        for child in result['items']:
+            if child['kind'] != 'drive#file':
+                print 'Unknown object type (not file or folder): "%s"' % child['kind']
+                pp(child)
+
+            if child['mimeType'] == 'application/vnd.google-apps.folder':
+                data_new_folder = {
+                  'title': child['title'],
+                  'parents': [{ 'id': fID_to }],
+                  'mimeType': 'application/vnd.google-apps.folder',
+                }
+
+                exists_check = gauth.service.files().list(q='title = "%s" and "%s" in parents and trashed = false' % (child['title'], fID_to)).execute()
+                pp(exists_check)
+
+                if exists_check['items'] == []:
+                    print '  ' * (__currentDepth+1) + 'Trying to create folder "%s"' % child['title']
+                    new_folder = gauth.service.files().insert(body=data_new_folder).execute()
+                    print '  ' * (__currentDepth+1) + 'Created folder "%s" (id: %s) in "%s" (id: %s)' % (new_folder['title'], new_folder['id'], child['title'], child['id'])
+                else:
+                    new_folder = exists_check['items'][0]
+                recursiveCopyInto(gauth, child['id'], new_folder['id'], maxdepth=maxdepth, __currentDepth=__currentDepth+1)
+
+            else:
+                copied_file = {
+                  'parents': [{ 'id': fID_to }], # Place copy in "to" folder
+                  'title': child['title'], # If `title` is not specified the new file will be named
+                                           # "Copy of ..."
+                }
+
+                exists_check = gauth.service.files().list(q='title = "%s" and "%s" in parents and trashed = false' % (child['title'], fID_to)).execute()
+
+                if exists_check['items'] == []:
+                    print '  ' * (__currentDepth+1) + 'Trying to copy "%s"' % child['title']
+                    try:
+                        new_file = gauth.service.files().copy(fileId=child['id'], body=copied_file).execute()
+                        print '  ' * (__currentDepth+1) + 'Copied file "%s"' % new_file['title']
+                    except googleapiclient.errors.HttpError as e:
+                        print 'Failed: %s\n trying again' % e
+                        new_file = gauth.service.files().copy(fileId=child['id'], body=copied_file).execute()
+                else:
+                    print 'file "%s" already exists in destination folder "%s"' % (child['title'], fID_to)
+
+
+        # Get page
+        page_token = result.get('nextPageToken')
+        if not page_token:
+            break
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Recursively copies the contents of a Google Drive folder to another Google Drive folder')
+    parser.add_argument('to_folder_id', metavar='FROM_FOLDER_ID', help='an integer for the accumulator')
+    parser.add_argument('from_folder_id', metavar='TO_FOLDER_ID', help='an integer for the accumulator')
+
+    args = parser.parse_args()
+
+    # Authenticate
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+
+    drive = GoogleDrive(gauth)
+    gauth.Authorize()
+
+    # Copy folder
+    recursiveCopyInto(gauth, args.to_folder_id, args.from_folder_id) #, maxdepth=0)
+
+
+# permission_list = gauth.service.permissions().list(fileId=file1['id']).execute()
+# print 'Changing permission for file "%s" (id: %s)' % (file1['title'], file1['id'])
+
+# new_permission = {
+#   'role': 'owner',
+#   'type': 'user',
+#   'value': 'malthe@socialsquare.dk',
+# }
+
+# # print gauth.service.permissions().update(fileId=file1['id']).execute()
+
+# # Try to change permission
+# # * Ownership can only be transferred to another user in the same domain # as the current owner.  # (https://support.google.com/drive/answer/2494892?hl=en)
+# # * You cannot change permission between personal and Google Apps account (https://productforums.google.com/d/msg/docs/5liCUAfvKUs/y9h7GYQT_z0J)
+# #
+# ### gauth.service.permissions().insert(fileId=file1['id'], body=new_permission).execute()
+
+# copied_file = {
+#   'parents': [{ 'id': '0B-WpRvMKai6WfkxCcXh2UUhVUktjT0dnUEZkdEtaY2E1UUo2dUctTnhIMmxUelVQN3FzZ3M' }], # "Copied from BIT BLUEPRINT" folder
+#   # 'title': file1.metadata['title'] + ' copy',
+# }
+
+# new_file = gauth.service.files().copy(fileId=file1['id'], body=copied_file).execute()
+# pp(new_file)
+
+# # permission_list = gauth.service.permissions().list(fileId=file1['id']).execute()
+# # pp(permissions['items'])
